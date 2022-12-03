5493ced4b859fab26df9036d82ef6a01beb3f22b
CloudyShoe <cloudyshoe@gmail.com>
2022-12-03 03:42:28 -0500

    update findRepeat to work with arbitrary length array

--- 03/03.js
+++ 03/03.js
@@ -19,22 +19,20 @@ const inputArr = input.split("\n");
 const resultOne = inputArr.map(e => [e.slice(0, e.length/2), e.slice(e.length/2, e.length)]).map(e => findRepeat(e)).map(e => priority.indexOf(e)).reduce((a,b) => a +b);
 document.getElementById('answer1').textContent = resultOne;
 
-function findRepeat(arr) {
-	let arr1 = arr[0];
-	let arr2 = arr[1];
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr2.includes(arr1[i])) {
-			return arr1[i];
+ function findRepeat(arr) {
+	for (let i = 0; i < arr[1].length; i++) {
+		if (arr.every(e => e.includes(arr[1][i]))) {
+			return arr[1][i];
 		}
 	}
 }
 
 const exampleElves = groupElves(exampleArr);
-const exampleResultTwo = exampleElves.map(e => findRepeatThree(e)).map(e => priority.indexOf(e)).reduce((a,b) => a + b);
+const exampleResultTwo = exampleElves.map(e => findRepeat(e)).map(e => priority.indexOf(e)).reduce((a,b) => a + b);
 console.log(exampleResultTwo);
 
 const elves = groupElves(inputArr);
-const resultTwo = elves.map(e => findRepeatThree(e)).map(e => priority.indexOf(e)).reduce((a,b) => a + b);
+const resultTwo = elves.map(e => findRepeat(e)).map(e => priority.indexOf(e)).reduce((a,b) => a + b);
 document.getElementById('answer2').textContent = resultTwo;
 
 function groupElves(arr) {
@@ -48,15 +46,4 @@ function groupElves(arr) {
 		};
 	}
 	return retArr;
-}
-
-function findRepeatThree(arr) {
-	let arr1 = arr[0];
-	let arr2 = arr[1];
-	let arr3 = arr[2];
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr2.includes(arr1[i]) && arr3.includes(arr1[i])) {
-			return arr1[i];
-		}
-	}
 }
